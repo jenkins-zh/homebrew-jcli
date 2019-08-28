361874ad77e098a17592a1a72bed05a2a62165e8
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-08-28 08:21:58 +0800

    Upgrade to 0.0.19

--- Formula/jcli.rb
+++ Formula/jcli.rb
@@ -1,7 +1,7 @@
 class Jcli < Formula
   desc "Jenkins CLI which support you"
   homepage "https://jenkins-zh.cn"
-  version "0.0.18"
+  version "0.0.19"
   
   url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v#{version}/jcli-darwin-amd64.tar.gz"
   # depends_on "cmake" => :build
