31e6f851018ac8fb787c02ff6e3769b33aa3b8ba
rick <rick@jenkins-zh.cn>
2019-10-04 21:09:41 +0800

    Fix the brew audit issues

--- Formula/jcli.rb
+++ Formula/jcli.rb
@@ -1,20 +1,18 @@
 class Jcli < Formula
   desc "Jenkins CLI allows you manage your Jenkins as an easy way"
-  homepage "https://jenkins-zh.cn"
-  version "0.0.20"
+  homepage "https://jenkins-zh.github.io"
+  url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.20/jcli-darwin-amd64.tar.gz"
   sha256 "644190a442500269e9c097f59244da1d40cfcd54af110d829aee78de360df902"
-  
-  url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v#{version}/jcli-darwin-amd64.tar.gz"
 
   def install
     bin.install name
 
     prefix.install_metafiles
   end
-  
+
   test do
     # test version CLI command
-    version_output = shell_output("#{bin}/v -v")
+    version_output = shell_output("#{bin}/jcli -v")
     assert_match version.to_s, version_output
   end
 end
