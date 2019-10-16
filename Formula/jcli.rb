cb50eb43645f6e05be6ac8e61f624cec17df41d2
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-10-16 12:29:27 +0800

    Release v0.0.21

--- Formula/jcli.rb
+++ Formula/jcli.rb
@@ -1,8 +1,8 @@
 class Jcli < Formula
   desc "Jenkins CLI allows you manage your Jenkins as an easy way"
-  homepage "https://jenkins-zh.github.io"
-  url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.20/jcli-darwin-amd64.tar.gz"
-  sha256 "644190a442500269e9c097f59244da1d40cfcd54af110d829aee78de360df902"
+  homepage "https://jenkins-zh.github.io/jcli-docs/"
+  url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.21/jcli-darwin-amd64.tar.gz"
+  sha256 "6e30f65d8cb5d4c27061378f1ee4eeb682e08440203c4928b78350cd57d55cb4"
 
   def install
     bin.install name
