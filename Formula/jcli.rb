f6a6f095d1425604ef4e0f56d8f3cc304a46f384
rick <rick@jenkins-zh.cn>
2019-10-31 21:07:04 +0800

    Bump to v0.0.22

--- Formula/jcli.rb
+++ Formula/jcli.rb
@@ -1,11 +1,15 @@
 class Jcli < Formula
   desc "Jenkins CLI allows you manage your Jenkins as an easy way"
   homepage "https://jenkins-zh.github.io"
-  url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.20/jcli-darwin-amd64.tar.gz"
-  sha256 "644190a442500269e9c097f59244da1d40cfcd54af110d829aee78de360df902"
+  url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.22/jcli-darwin-amd64.tar.gz"
+  sha256 "dcd330dcd7777d46d35af2637c8fa3dcdcd6ff8ca883af182818a7d514eafac0"
 
   def install
     bin.install name
+
+    # Install bash completion
+    output = Utils.popen_read("#{bin}/jcli completion")
+    (bash_completion/"jcli").write output
 
     prefix.install_metafiles
   end
