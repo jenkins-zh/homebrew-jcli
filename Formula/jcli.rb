0ae31df4ebc77e70cd0e16a4c445b13e8b6bf208
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-23 10:08:27 +0800

    Brew formula update for jcli version v0.0.35

--- Formula/jcli.rb
+++ Formula/jcli.rb
@@ -5,20 +5,20 @@
 class Jcli < Formula
   desc "Jenkins CLI allows you manage your Jenkins as an easy way"
   homepage "https://github.com/jenkins-zh/jenkins-cli"
-  version "0.0.34"
+  version "0.0.35"
   bottle :unneeded
 
-  if OS.mac?
-    url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.34/jcli-darwin-amd64.tar.gz"
-    sha256 "1875a8e4430fa446ebb95bf91f791c9f3f3695f4ad2eb81da2afcab20e3c1689"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.35/jcli-darwin-amd64.tar.gz"
+    sha256 "12cd78a5c890b3db43b1522b0bb0ba1644b49686ec4cbc91abc4411e37cbfc42"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.34/jcli-linux-amd64.tar.gz"
-    sha256 "018bb3fd3a4db434d84f816b4eab818aaafe27ccc776f466c742c678991115ea"
+    url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.35/jcli-linux-amd64.tar.gz"
+    sha256 "405fd6362e5d6dfb6faae2a2d679b6a2c9ec6a785a0f9791f0d31035acd75c47"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.34/jcli-linux-arm64.tar.gz"
-    sha256 "76828fed9f1e13337f6a23b3113887251e5152eeaadb6a7f72f408bf71b368fe"
+    url "https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.35/jcli-linux-arm64.tar.gz"
+    sha256 "ce15f6f985b0560f4e41961a7d906577ffc2be1d84b33993357b26f91b08c445"
   end
 
   depends_on "vim" => :optional
